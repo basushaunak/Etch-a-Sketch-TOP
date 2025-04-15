b822ac8ebd8a1aa1acb7a265a055f825c0c5bef3
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-04-15 06:34:46 +0530

    Grid population work done, need to work on mouse hovering

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector("#grid-container");
+const resetButton = document.querySelector("#reset-button");
 
 function getRandomInteger(x, y) {
     return Math.floor(Math.random() * (y - x + 1)) + x;
@@ -12,10 +13,15 @@ function getRandomRGB(){
 }
 
 function populateGrid(columnCount){
-    let cellSize = 640/columnCount;
+    let cellSize = gridContainer.clientHeight/columnCount;
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.setAttribute("style", "min-height: " + cellSize + "px;min-width: " + cellSize + "px;");
+    let newCell = cell.cloneNode(false);
     for(let i=0;i<columnCount;i++){
         for(let j=0;j<columnCount;j++){
-            
+            gridContainer.appendChild(newCell);
+            newCell = cell.cloneNode(false);
         }
     }    
 }
@@ -23,3 +29,10 @@ function populateGrid(columnCount){
 window.onload = ()=>{
     populateGrid(64);
 }
+
+gridContainer.addEventListener("mouseover",(e)=>{
+    // e.target.setAttribute("style","background-color: "+getRandomRGB());
+    let style = window.getComputedStyle(e.target);
+    console.log(e.target);
+    console.log(style.getPropertyValue('opacity'));
+})
